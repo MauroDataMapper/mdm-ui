f4f685a52574e2681bfb3b401a219bd7090bfca3
soheilsaif <soheil.saifipour@ndm.ox.ac.uk>
2019-04-21 12:06:17 +0100

    Add jQuery into Tests

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,7 @@ module.exports = function(config) {
         exclude: [],
         //files/patterns to load in the browser
         files: [
+            './node_modules/jquery/dist/jquery.js',
             './node_modules/angular/angular.js',
             './node_modules/angular-ui-router/release/angular-ui-router.js',
             './node_modules/angular-mocks/angular-mocks.js',

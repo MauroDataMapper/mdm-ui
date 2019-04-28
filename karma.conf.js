f9608c63a282738ff8aa4b072aa361357dcb54f6
soheilsaif <soheil.saifipour@ndm.ox.ac.uk>
2019-04-28 10:36:57 +0100

    Add junit jenkins reporter for karma

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,20 @@ module.exports = function(config) {
         //list of browsers to launch and capture
         browsers: ['Chrome',/*'PhantomJS','Firefox','Edge','ChromeCanary','Opera','IE','Safari'*/],
         //list of reporters to use
-        reporters: ['mocha','kjhtml'/*,'dots','progress','spec'*/],
+        reporters: ['mocha','kjhtml'/*,'dots','progress','spec', 'junit'*/],
+
+
+        // the default configuration
+        junitReporter: {
+            outputDir: 'test_results', // results will be saved as $outputDir/$browserName.xml
+            outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+            suite: '', // suite will become the package name attribute in xml testsuite element
+            useBrowserName: true, // add browser name to report and classes names
+            nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+            classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+            properties: {}, // key value pair of properties to add to the <properties> section of the report
+            xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+        },
 
         //address that the server will listen on, '0.0.0.0' is default
         listenAddress: '0.0.0.0',

6653254825301284900b929f0341e6196ccbf0ce
Gabriel Ignat <gabriel.ignat@bdi.ox.ac.uk>
2020-03-16 09:41:05 +0000

    WIP: Changed the footer to always stay at the bootom. Styling pending

--- src/app/shared/ui-view/ui-view.component.ts
+++ src/app/shared/ui-view/ui-view.component.ts
@@ -2,6 +2,6 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mdm-ui-view',
-  template: '<div ui-view></div>'
+  template: '<div ui-view id="mdm-ui"></div>'
 })
 export class UiViewComponent {}

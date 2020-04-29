76060cf056463c3194e851732720ad4b064c1f64
Gabriel Ignat <gabriel.ignat@bdi.ox.ac.uk>
2020-04-29 09:13:24 +0100

    WIP: Grouped the SCSS code. Code refactoring. Updated the terminology details page

--- src/app/admin/users-table/users-table.component.ts
+++ src/app/admin/users-table/users-table.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChildren, ViewChild, EventEmitter, ElementRef, AfterViewInit} from '@angular/core';
+import { Component, OnInit, ViewChildren, ViewChild, EventEmitter, ElementRef, AfterViewInit } from '@angular/core';
 import { MessageHandlerService } from '@mdm/services/utility/message-handler.service';
 import { ResourcesService } from '@mdm/services/resources.service';
 import { merge, Observable, from } from 'rxjs';
@@ -6,6 +6,8 @@ import { catchError, map, startWith, switchMap } from 'rxjs/operators';
 import { StateHandlerService } from '@mdm/services/handlers/state-handler.service';
 import { MatSort } from '@angular/material/sort';
 import { MdmPaginatorComponent } from '@mdm/shared/mdm-paginator/mdm-paginator';
+import { BroadcastService } from '@mdm/services/broadcast.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'mdm-users-table',
@@ -30,9 +32,17 @@ export class UsersTableComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['username', 'emailAddress', 'organisation', 'userRole', 'groups', 'status', 'icons'];
   records: any[] = [];
 
-  constructor(private messageHandler: MessageHandlerService, private resources: ResourcesService, private stateHandler: StateHandlerService) {}
+  constructor(
+    private messageHandler: MessageHandlerService,
+    private resources: ResourcesService,
+    private stateHandler: StateHandlerService,
+    private broadcastSvc: BroadcastService,
+    private title: Title
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.title.setTitle('Manage users');
+  }
 
   ngAfterViewInit() {
     this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
@@ -41,24 +51,32 @@ export class UsersTableComponent implements OnInit, AfterViewInit {
     merge(this.sort.sortChange, this.paginator.page, this.filterEvent).pipe(startWith({}), switchMap(() => {
           this.isLoadingResults = true;
 
-          return this.usersFetch(this.paginator.pageSize, this.paginator.pageOffset, this.sort.active, this.sort.direction, this.filter);
-        }),
-        map((data: any) => {
+          return this.usersFetch(
+            this.paginator.pageSize,
+            this.paginator.pageOffset,
+            this.sort.active,
+            this.sort.direction,
+            this.filter
+          );
+        }), map((data: any) => {
           this.totalItemCount = data.body.count;
           this.isLoadingResults = false;
           return data.body.items;
-        }),
-        catchError(() => {
+        }), catchError(() => {
           this.isLoadingResults = false;
           return [];
-        })
-      )
-      .subscribe(data => {
+        })).subscribe((data) => {
         this.records = data;
       });
   }
 
-  usersFetch(pageSize?, pageIndex?, sortBy?, sortType?, filters?): Observable<any> {
+  usersFetch(
+    pageSize?,
+    pageIndex?,
+    sortBy?,
+    sortType?,
+    filters?
+  ): Observable<any> {
     const options = { pageSize, pageIndex, sortBy, sortType, filters };
 
     return this.resources.catalogueUser.get(null, null, options);
@@ -66,7 +84,7 @@ export class UsersTableComponent implements OnInit, AfterViewInit {
 
   editUser(row) {
     if (row) {
-      this.stateHandler.Go('admin.user', { id: row.id, }, null);
+      this.stateHandler.Go('admin.user', { id: row.id }, null);
     }
   }
 
@@ -83,8 +101,7 @@ export class UsersTableComponent implements OnInit, AfterViewInit {
   resetPassword(row) {
     from(this.resources.catalogueUser.put(row.id, 'adminPasswordReset', null)).subscribe(() => {
         this.messageHandler.showSuccess('Reset password email sent successfully!');
-      },
-      error => {
+      }, error => {
         this.messageHandler.showError('There was a problem sending reset password email.', error);
       }
     );
@@ -94,8 +111,8 @@ export class UsersTableComponent implements OnInit, AfterViewInit {
     row.disabled = !row.disabled;
     from(this.resources.catalogueUser.put(row.id, null, { resource: row })).subscribe(() => {
         this.messageHandler.showSuccess('User details updated successfully.');
-      },
-      error => {
+        this.broadcastSvc.broadcast('pendingUserUpdated');
+      }, error => {
         this.messageHandler.showError('There was a problem updating the user.', error);
       }
     );
@@ -117,5 +134,5 @@ export class UsersTableComponent implements OnInit, AfterViewInit {
 
   filterClick = () => {
     this.hideFilters = !this.hideFilters;
-  }
+  };
 }

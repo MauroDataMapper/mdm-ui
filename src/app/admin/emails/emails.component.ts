850b021b0907cba3536802a0aacd52afee131439
Anshita <shrivastavacold@gmail.com>
2020-05-12 12:11:00 +0100

    Email component- Fixed grid pagination/filter/sorting

--- src/app/admin/emails/emails.component.ts
+++ src/app/admin/emails/emails.component.ts
@@ -10,9 +10,21 @@ import {
 import { MatSort } from '@angular/material/sort';
 import { MessageHandlerService } from '@mdm/services/utility/message-handler.service';
 import { ResourcesService } from '@mdm/services/resources.service';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
+import {merge, Observable} from 'rxjs';
+import {catchError, map, startWith, switchMap} from 'rxjs/operators';
+import {MdmPaginatorComponent} from '@mdm/shared/mdm-paginator/mdm-paginator';
 import { Title } from '@angular/platform-browser';
+
+// import { Component, OnInit, ViewChildren, ViewChild, EventEmitter, ElementRef, AfterViewInit } from '@angular/core';
+// import { MessageHandlerService } from '@mdm/services/utility/message-handler.service';
+// import { ResourcesService } from '@mdm/services/resources.service';
+// import { merge, Observable, from } from 'rxjs';
+// import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+// import { StateHandlerService } from '@mdm/services/handlers/state-handler.service';
+// import { MatSort } from '@angular/material/sort';
+// import { MdmPaginatorComponent } from '@mdm/shared/mdm-paginator/mdm-paginator';
+// import { BroadcastService } from '@mdm/services/broadcast.service';
+// import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'mdm-app-emails',
@@ -21,14 +33,26 @@ import { Title } from '@angular/platform-browser';
 })
 export class EmailsComponent implements OnInit, AfterViewInit {
   @ViewChildren('filters', { read: ElementRef }) filters: ElementRef[];
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
-
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MdmPaginatorComponent, { static: true }) paginator: MdmPaginatorComponent;
   filterEvent = new EventEmitter<string>();
   hideFilters = true;
   isLoadingResults: boolean;
   totalItemCount = 0;
   filter: string;
+  // @ViewChildren('filters', { read: ElementRef }) filters: ElementRef[];
+  // @ViewChild(MatSort, { static: false }) sort: MatSort;
+  // @ViewChild(MdmPaginatorComponent, { static: true }) paginator: MdmPaginatorComponent;
+  //
+  // filterEvent = new EventEmitter<string>();
+  // hideFilters = true;
+  // filter: string;
+  // isLoadingResults: boolean;
+  // totalItemCount = 0;
+  // deleteInProgress: boolean;
+  // processing: boolean;
+  // failCount: number;
+  // total: number;
 
   records: any[] = [];
   displayedColumns = [
@@ -38,81 +62,127 @@ export class EmailsComponent implements OnInit, AfterViewInit {
     'successfullySent'
   ];
 
-  dataSource = new MatTableDataSource<any>();
+  // dataSource = new MatTableDataSource<any>();
 
   constructor(
     private messageHandler: MessageHandlerService,
     private resourcesService: ResourcesService,
     private title: Title
+
   ) {
-    this.dataSource = new MatTableDataSource(this.records);
+    // this.dataSource = new MatTableDataSource(this.records);
   }
 
   ngOnInit() {
-    this.mailsFetch();
+    // this.mailsFetch();
     this.title.setTitle('Emails');
   }
 
   ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+    this.filterEvent.subscribe(() => (this.paginator.pageIndex = 0));
 
-    this.dataSource.sortingDataAccessor = (item, property) => {
-      if (property === 'sentToEmailAddress') {
-        return item.sentToEmailAddress;
-      }
+    merge(this.sort.sortChange, this.paginator.page, this.filterEvent).pipe(startWith({}), switchMap(() => {
+        this.isLoadingResults = true;
 
-      if (property === 'dateTimeSent') {
-        return item.dateTimeSent;
-      }
+        return this.mailsFetch(this.paginator.pageSize, this.paginator.pageOffset, this.sort.active, this.sort.direction, this.filter);
+      }),
+      map((data: any) => {
+        this.totalItemCount = data.body.count;
+        this.isLoadingResults = false;
+        return data.body.items;
+        //   .forEach(row => {
+        //   row.dateTimeSentString =
+        //     row.dateTimeSent.year +
+        //     '/' +
+        //     row.dateTimeSent.monthValue +
+        //     '/' +
+        //     row.dateTimeSent.dayOfMonth +
+        //     ' ' +
+        //     row.dateTimeSent.hour +
+        //     ':' +
+        //     row.dateTimeSent.minute +
+        //     ':' +
+        //     row.dateTimeSent.second;
+        // });
 
-      if (property === 'subject') {
-        return item.subject;
-      }
+      }),
+      catchError(() => {
+        this.isLoadingResults = false;
+        return [];
+      })).subscribe(data => {
+        this.records = data;
 
-      if (property === 'successfullySent') {
-        return item.successfullySent;
-      }
-    };
+      });
   }
 
-  mailsFetch(pageSize?, pageIndex?, sortBy?, sortType?, filters?) {
-    const options = {
-      pageSize,
-      pageIndex,
-      filters,
-      sortBy: 'sentToEmailAddress',
-      sortType: 'asc'
-    };
+  mailsFetch(pageSize?, pageIndex?, sortBy?, sortType?, filters?): Observable<any> {
+    const options = { pageSize, pageIndex, sortBy, sortType, filters };
 
-    this.resourcesService.admin.get('emails', options).subscribe(
-      resp => {
-        this.records = resp.body;
-        this.records.forEach(row => {
-          row.dateTimeSentString =
-            row.dateTimeSent.year +
-            '/' +
-            row.dateTimeSent.monthValue +
-            '/' +
-            row.dateTimeSent.dayOfMonth +
-            ' ' +
-            row.dateTimeSent.hour +
-            ':' +
-            row.dateTimeSent.minute +
-            ':' +
-            row.dateTimeSent.second;
-        });
-        this.totalItemCount = this.records.length;
-        this.refreshDataSource();
-      },
-      err => {
-        this.messageHandler.showError(
-          'There was a problem loading user emails.',
-          err
-        );
-      }
-    );
+    return this.resourcesService.admin.get('emails', options);
   }
+
+
+  // ngAfterViewInit() {
+  //   this.dataSource.sort = this.sort;
+  //   this.dataSource.paginator = this.paginator;
+  //
+  //   this.dataSource.sortingDataAccessor = (item, property) => {
+  //     if (property === 'sentToEmailAddress') {
+  //       return item.sentToEmailAddress;
+  //     }
+  //
+  //     if (property === 'dateTimeSent') {
+  //       return item.dateTimeSent;
+  //     }
+  //
+  //     if (property === 'subject') {
+  //       return item.subject;
+  //     }
+  //
+  //     if (property === 'successfullySent') {
+  //       return item.successfullySent;
+  //     }
+  //   };
+  // }
+
+  // mailsFetch(pageSize?, pageIndex?, sortBy?, sortType?, filters?) {
+  //   const options = {
+  //     pageSize,
+  //     pageIndex,
+  //     filters,
+  //     sortBy: 'sentToEmailAddress',
+  //     sortType: 'asc'
+  //   };
+  //
+  //   this.resourcesService.admin.get('emails', options).subscribe(
+  //     resp => {
+  //       this.records = resp.body.items;
+  //       this.records.forEach(row => {
+  //         row.dateTimeSentString =
+  //           row.dateTimeSent.year +
+  //           '/' +
+  //           row.dateTimeSent.monthValue +
+  //           '/' +
+  //           row.dateTimeSent.dayOfMonth +
+  //           ' ' +
+  //           row.dateTimeSent.hour +
+  //           ':' +
+  //           row.dateTimeSent.minute +
+  //           ':' +
+  //           row.dateTimeSent.second;
+  //       });
+  //       this.totalItemCount = this.records.length;
+  //       this.refreshDataSource();
+  //     },
+  //     err => {
+  //       this.messageHandler.showError(
+  //         'There was a problem loading user emails.',
+  //         err
+  //       );
+  //     }
+  //   );
+  // }
 
   applyFilter = () => {
     let filter: any = '';
@@ -137,6 +207,6 @@ export class EmailsComponent implements OnInit, AfterViewInit {
   }
 
   refreshDataSource() {
-    this.dataSource.data = this.records;
+    // this.dataSource.data = this.records;
   }
 }

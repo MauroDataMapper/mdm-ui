134395247b5a24eb9e60a57eba1cf5bcd5609e48
Ollie Freeman <ollie.freeman@gmail.com>
2020-08-12 09:57:14 +0100

    Next snapshot

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -17,7 +17,7 @@ SPDX-License-Identifier: Apache-2.0
 */
 export const environment = {
   production: true,
-  version: '4.0.2',
+  version: '4.0.3-SNAPSHOT',
   apiEndpoint: 'api',
   HDFLink: '',
   appTitle: 'Metadata Catalogue',

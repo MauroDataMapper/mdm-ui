84eaae7f3f9d33d0d3a513e46a64ec64d78e6942
Andrew Tsui <a.tpw25@googlemail.com>
2020-08-21 11:36:30 +0100

    build: Fixed `jest` configuration

--- src/setupJest.ts
+++ src/setupJest.ts
@@ -15,5 +15,5 @@ limitations under the License.
 
 SPDX-License-Identifier: Apache-2.0
 */
-// import 'jest-preset-angular';
+import 'jest-preset-angular';
 // import './jestGlobalMocks'; // browser mocks globally available for every test

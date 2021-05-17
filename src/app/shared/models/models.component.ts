7221aea5c5d01e054935ba6d1c89ea7f3216f865
James Welch <james.welch@cs.ox.ac.uk>
2021-05-17 10:59:47 +0100

    Update NOTICE / headers Add script for running license check

--- src/app/shared/models/models.component.ts
+++ src/app/shared/models/models.component.ts
@@ -1,5 +1,6 @@
 /*
 Copyright 2020-2021 University of Oxford
+and Health and Social Care Information Centre, also known as NHS Digital
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -15,6 +16,7 @@ limitations under the License.
 
 SPDX-License-Identifier: Apache-2.0
 */
+
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { BroadcastService } from '@mdm/services/broadcast.service';

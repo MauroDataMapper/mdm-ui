4e0ee5286a6734a6165e01c2f65d2de450407625
Pete <peter.gammon@oxfordcc.co.uk>
2021-05-18 15:26:00 +0100

    Licence issue resolved

--- src/app/utility/download-link/download-link.component.ts
+++ src/app/utility/download-link/download-link.component.ts
@@ -1,6 +1,6 @@
-
 /*
 Copyright 2020-2021 University of Oxford
+and Health and Social Care Information Centre, also known as NHS Digital
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-SPDX-License
+SPDX-License-Identifier: Apache-2.0
 */
 
 import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';

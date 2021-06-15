5227bba2c3ea53f0f7c8f722747d61341402516a
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2021-06-15 17:08:45 +0100

    #178 Update Code Set wizard to use reactive forms

--- src/app/wizards/codeSet/code-set-main/code-set-main.component.ts
+++ src/app/wizards/codeSet/code-set-main/code-set-main.component.ts
@@ -26,6 +26,7 @@ import { CatalogueItemDomainType, CodeSetCreatePayload, CodeSetDetailResponse, C
 import { FolderService } from '@mdm/folders-tree/folder.service';
 import { catchError } from 'rxjs/operators';
 import { EMPTY } from 'rxjs';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mdm-code-set-main',
@@ -37,14 +38,39 @@ export class CodeSetMainComponent implements OnInit {
   parentDomainType: CatalogueItemDomainType;
   parentFolder: Container;
   savingInProgress = false;
-  model = {
-    label: '',
-    author: '',
-    organisation: '',
-    description: '',
-    classifiers: [],
-    terms: [],
-  };
+  setupForm: FormGroup;
+
+  get label() {
+    return this.setupForm.get('label');
+  }
+
+  get author() {
+    return this.setupForm.get('author');
+  }
+
+  get organisation() {
+    return this.setupForm.get('organisation');
+  }
+
+  get description() {
+    return this.setupForm.get('description');
+  }
+
+  get classifiers() {
+    return this.setupForm.get('classifiers');
+  }
+
+  set classifiersValue(value: any[]) {
+    this.classifiers.setValue(value);
+  }
+
+  get terms() {
+    return this.setupForm.get('terms');
+  }
+
+  set termsValue(value: any[]) {
+    this.terms.setValue(value);
+  }
 
   constructor(
     private uiRouterGlobals: UIRouterGlobals,
@@ -56,6 +82,15 @@ export class CodeSetMainComponent implements OnInit {
 
   ngOnInit() {
     this.title.setTitle('New Code Set');
+
+    this.setupForm = new FormGroup({
+      label: new FormControl('', Validators.required),  // eslint-disable-line @typescript-eslint/unbound-method
+      author: new FormControl('', Validators.required), // eslint-disable-line @typescript-eslint/unbound-method
+      organisation: new FormControl('', Validators.required), // eslint-disable-line @typescript-eslint/unbound-method
+      description: new FormControl(''),
+      classifiers: new FormControl([]),
+      terms: new FormControl([], Validators.required) // eslint-disable-line @typescript-eslint/unbound-method
+    });
 
     this.parentFolderId = this.uiRouterGlobals.params.parentFolderId;
     this.parentDomainType = this.uiRouterGlobals.params.parentDomainType;
@@ -74,29 +109,31 @@ export class CodeSetMainComponent implements OnInit {
   }
 
   save() {
-    if (this.model.label && this.model.author && this.model.organisation && this.model.terms.length > 0) {
-      const resource: CodeSetCreatePayload = {
-        label: this.model.label,
-        author: this.model.author,
-        organisation: this.model.organisation,
-        description: this.model.description,
-        classifiers: this.model.classifiers,
-        folder: this.parentFolderId,
-        terms: this.model.terms
-      };
-
-      this.resources.codeSet
-        .addToFolder(this.parentFolderId, resource)
-        .pipe(
-          catchError(error => {
-            this.messageHandler.showError('There was a problem creating the Code Set.', error);
-            return EMPTY;
-          })
-        )
-        .subscribe((response: CodeSetDetailResponse) => {
-          this.messageHandler.showSuccess('Code Set created successfully.');
-          this.stateHandler.Go('codeset', { id: response.body.id }, { reload: true });
-        });
+    if (!this.setupForm.valid) {
+      return;
     }
+
+    const resource: CodeSetCreatePayload = {
+      label: this.label.value,
+      author: this.author.value,
+      organisation: this.organisation.value,
+      description: this.description.value,
+      classifiers: this.classifiers.value,
+      folder: this.parentFolderId,
+      terms: this.terms.value
+    };
+
+    this.resources.codeSet
+      .addToFolder(this.parentFolderId, resource)
+      .pipe(
+        catchError(error => {
+          this.messageHandler.showError('There was a problem creating the Code Set.', error);
+          return EMPTY;
+        })
+      )
+      .subscribe((response: CodeSetDetailResponse) => {
+        this.messageHandler.showSuccess('Code Set created successfully.');
+        this.stateHandler.Go('codeset', { id: response.body.id }, { reload: true });
+      });
   }
 }

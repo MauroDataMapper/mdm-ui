b2181dcc184801d08c8fe58523eabcc1f647aba6
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2021-06-16 16:48:06 +0100

    MC-9549 UI updates to NewVersionComponent
    * Reword text depending on if a folder or a model is being versioned
    * Replace radio check boxes with select dropdown

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -43,6 +43,7 @@ import { TextDiffComponent } from '@mdm/shared/text-diff/text-diff.component';
 import { ElementLinkComponent } from '@mdm/utility/element-link/element-link.component';
 import { DownloadLinkComponent } from '@mdm/utility/download-link/download-link.component';
 import { NewVersionComponent } from '@mdm/shared/new-version/new-version.component';
+import { AlertComponent } from '@mdm/shared/alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -61,7 +62,8 @@ import { NewVersionComponent } from '@mdm/shared/new-version/new-version.compone
     TextDiffComponent,
     ElementLinkComponent,
     DownloadLinkComponent,
-    NewVersionComponent
+    NewVersionComponent,
+    AlertComponent
   ],
   imports: [
     CommonModule,
@@ -101,7 +103,8 @@ import { NewVersionComponent } from '@mdm/shared/new-version/new-version.compone
     ElementLinkComponent,
     TextDiffComponent,
     DownloadLinkComponent,
-    NewVersionComponent
+    NewVersionComponent,
+    AlertComponent
   ]
 })
 export class SharedModule {}

2c3c6e707291b5b4e3f87172268826f0d9c9d258
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2021-06-23 14:23:35 +0100

    MC-9573 Add guard to check OpenID Connect plugin is installed

--- src/app/admin/openid-connect-provider-table/openid-connect-provider-table.component.ts
+++ src/app/admin/openid-connect-provider-table/openid-connect-provider-table.component.ts
@@ -17,6 +17,12 @@ limitations under the License.
 SPDX-License-Identifier: Apache-2.0
 */
 import { Component, OnInit } from '@angular/core';
+import { ModulesResponse } from '@maurodatamapper/mdm-resources';
+import { OpenIdConnectModuleName } from '@mdm/model/openid-connect.model';
+import { MdmResourcesService } from '@mdm/modules/resources';
+import { MessageHandlerService, SharedService, StateHandlerService } from '@mdm/services';
+import { EMPTY } from 'rxjs';
+import { catchError, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'mdm-openid-connect-provider-table',
@@ -25,9 +31,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpenidConnectProviderTableComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  moduleLoaded = false;
+
+  constructor(
+    private shared: SharedService,
+    private stateHandler: StateHandlerService,
+    private resources: MdmResourcesService,
+    private messageHandler: MessageHandlerService) { }
 
   ngOnInit(): void {
+    if (!this.shared.features.useOpenIdConnect) {
+      this.stateHandler.Go('home');
+      return;
+    }
+
+    this.loading = true;
+
+    this.resources.admin
+      .modules()
+      .pipe(
+        catchError(error => {
+          this.messageHandler.showError('There was a problem getting the installed modules.', error);
+          return EMPTY;
+        }),
+        finalize(() => this.loading = false)
+      )
+      .subscribe((response: ModulesResponse) => {
+        this.moduleLoaded = response.body.findIndex(module => module.name === OpenIdConnectModuleName) !== -1;
+        if (this.moduleLoaded) {
+          this.initialise();
+        }
+      });
   }
 
+  private initialise() {
+
+  }
 }

22d4c15f671f82ce6a500c2c1dea715d37325ece
Andrew Tsui <a.tpw25@googlemail.com>
2021-07-30 10:49:58 +0100

    fix: Update to better handle null exception.

--- src/app/dataClass/data-class/data-class.component.ts
+++ src/app/dataClass/data-class/data-class.component.ts
@@ -16,7 +16,7 @@ limitations under the License.
 
 SPDX-License-Identifier: Apache-2.0
 */
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MdmResourcesService } from '@mdm/modules/resources';
 import { MessageService } from '@mdm/services/message.service';
 import { SharedService } from '@mdm/services/shared.service';
@@ -47,7 +47,7 @@ import { BaseComponent } from '@mdm/shared/base/base.component';
 })
 export class DataClassComponent
   extends BaseComponent
-  implements OnInit, AfterViewInit {
+  implements OnInit, AfterViewInit, AfterViewChecked {
   @ViewChild('tab', { static: false }) tabGroup: MatTabGroup;
   dataClass: DataClassDetail;
   showSecuritySection: boolean;
@@ -126,7 +126,13 @@ export class DataClassComponent
   }
 
   ngAfterViewInit(): void {
-    this.editingService.setTabGroupClickEvent(this.tabGroup);
+    // this.editingService.setTabGroupClickEvent(this.tabGroup);
+  }
+
+  ngAfterViewChecked(): void {
+    if (this.tabGroup && !this.editingService.isTabGroupClickEventHandled(this.tabGroup)) {
+      this.editingService.setTabGroupClickEvent(this.tabGroup);
+    }
   }
 
   dataClassDetails(model, id, parentDataClass?) {

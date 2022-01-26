1e7b6d301d20b95dab58817ceb78c7574a321356
James Welch <james.welch@cs.ox.ac.uk>
2022-01-26 15:35:04 +0000

    Remove validation constraints on codeset creation (Fixes #429)

--- src/app/wizards/codeSet/code-set-main/code-set-main.component.ts
+++ src/app/wizards/codeSet/code-set-main/code-set-main.component.ts
@@ -85,11 +85,11 @@ export class CodeSetMainComponent implements OnInit {
 
     this.setupForm = new FormGroup({
       label: new FormControl('', Validators.required),  // eslint-disable-line @typescript-eslint/unbound-method
-      author: new FormControl('', Validators.required), // eslint-disable-line @typescript-eslint/unbound-method
-      organisation: new FormControl('', Validators.required), // eslint-disable-line @typescript-eslint/unbound-method
+      author: new FormControl(''), // eslint-disable-line @typescript-eslint/unbound-method
+      organisation: new FormControl(''), // eslint-disable-line @typescript-eslint/unbound-method
       description: new FormControl(''),
       classifiers: new FormControl([]),
-      terms: new FormControl([], Validators.required) // eslint-disable-line @typescript-eslint/unbound-method
+      terms: new FormControl([]) // eslint-disable-line @typescript-eslint/unbound-method
     });
 
     this.parentFolderId = this.uiRouterGlobals.params.parentFolderId;

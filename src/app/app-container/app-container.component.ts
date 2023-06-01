fdf40177186d605ba2f75ac832ab1137eb423853
James Welch <james.welch@cs.ox.ac.uk>
2023-06-01 16:29:52 +0100

    WIP Interface changes

--- src/app/app-container/app-container.component.ts
+++ src/app/app-container/app-container.component.ts
@@ -20,6 +20,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mdm-app-container',
   templateUrl: './app-container.component.html',
-  styleUrls: ['./app-container.component.sass']
+  styleUrls: ['./app-container.component.scss']
 })
 export class AppContainerComponent { }

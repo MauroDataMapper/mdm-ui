709722a2b7d694d85a346521fc3db111f36daf58
James Welch <jamesrwelch@gmail.com>
2024-02-09 15:23:38 +0000

    More tweaking of dependencies

--- src/app/diagram/services/dataflow-datamodel-diagram.service.ts
+++ src/app/diagram/services/dataflow-datamodel-diagram.service.ts
@@ -17,7 +17,7 @@ SPDX-License-Identifier: Apache-2.0
 */
 import { BasicDiagramService } from './basic-diagram.service';
 import { Observable } from 'rxjs';
-import * as joint from 'jointjs';
+import * as joint from '@joint/core';
 import { MdmResourcesService } from '@mdm/modules/resources';
 import { MessageHandlerService } from '@mdm/services/utility/message-handler.service';
 
